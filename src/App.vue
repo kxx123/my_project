2adb1fc5beb9e66957f7c024886dbddf99074835
kxx <2857064872@qq.com>
2018-09-09 19:07:30 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style scoped>
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
